259b88085d7748175eca0e2d3a2ae16b9442e5e7
MHamiid <mmh699@outlook.com>
2021-01-22 21:03:57 +0200

    Switched ImGui Submodule to (docking branch)

--- Violet/src/Violet/ImGui/ImGuiLayer.cpp
+++ Violet/src/Violet/ImGui/ImGuiLayer.cpp
@@ -6,6 +6,8 @@
 //#include "glad/glad.h" //imgui_impl_opengl3.h already includes glad.h
 #include "backends/imgui_impl_opengl3.h"
 
+#include "GLFW/glfw3.h"
+
 namespace Violet {
 	ImGuiLayer* ImGuiLayer::s_ImGuiLayer = nullptr;
 	ImGuiLayer::ImGuiLayer() : Layer("ImGui Layer") 
@@ -23,15 +25,28 @@ namespace Violet {
 	   //Setup Dear ImGui context
 		VIO_CORE_INFO("Initializing ImGUI: {0}!", IMGUI_CHECKVERSION());
 		ImGui::CreateContext();
-    	ImGuiIO& io = ImGui::GetIO(); //(void)io;
-		//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+		ImGuiIO& io = ImGui::GetIO(); (void)io;
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
 		//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+		//io.ConfigViewportsNoAutoMerge = true;
+		//io.ConfigViewportsNoTaskBarIcon = true;
 
 		// Setup Dear ImGui style
 		ImGui::StyleColorsDark();
 		//ImGui::StyleColorsClassic();
 
-		
+		// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+		ImGuiStyle& style = ImGui::GetStyle();
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		{
+			style.WindowRounding = 0.0f;
+			style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+		}
+
+
+
 		// Setup Platform/Renderer backends
 		ImGui_ImplOpenGL3_Init("#version 410");
 
@@ -76,5 +91,18 @@ namespace Violet {
 		// Rendering
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+		ImGuiIO& io = ImGui::GetIO();
+		// Update and Render additional Platform Windows
+		// (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+		//  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		{
+			GLFWwindow* backup_current_context = glfwGetCurrentContext();
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+			glfwMakeContextCurrent(backup_current_context);
+		}
 	}
 }
