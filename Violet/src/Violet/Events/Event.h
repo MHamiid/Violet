1bd44e4a72bb1fd0dc9483e97b119f74f933f1ed
MHamiid <mmh699@outlook.com>
2021-04-18 16:40:14 +0200

    Fixed that EventDispatcher::dispatch() could reset the event handled status back to false

--- Violet/src/Violet/Events/Event.h
+++ Violet/src/Violet/Events/Event.h
@@ -57,8 +57,11 @@ namespace Violet{
 
 			//If the event and function type matches.
 			if (m_event.getEventType() == T::GetClassType()) {
-	
-				m_event.setEventHandleStatus(FUNCEvent(static_cast<T&>(m_event))); //Call the appropriate function, Casting Event type to passed template type, Set m_handled.
+				
+				bool eventHandled = m_event.getEventHandleStatus();
+				//Using or(|) instead of assignment(=) incase the dispatcher dispatched the same event type to multiple functions, once m_event handled status has been set to true by a one function's return, it will never be reset to false by the others
+				eventHandled |= FUNCEvent(static_cast<T&>(m_event));
+				m_event.setEventHandleStatus(eventHandled); //Call the appropriate function, Casting Event type to passed template type, Set m_handled.
 				return true;
 			}
 			return false;
